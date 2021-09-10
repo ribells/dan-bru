6149650521430bd983eb79d0e3c484f3170d2a92
ribells <bdcampbel01@risd.edu>
2021-09-10 12:01:13 -0400

    first commit

---
+++ www/app/main.js
@@ -0,0 +1,14 @@
+define(function (require) {
+    // Load any app-specific modules with a relative require call like:
+    //var sketch = require('mymodule.js');
+
+    // Load library/vendor modules using full IDs:
+    var print = require('print');
+    var THREE = require('three');
+    var terrain = require('terrain_vis');
+
+    var terrain = new terrain.Terrain (400, 300, THREE);
+    var mesh = terrain.generate_terrain();
+	
+    print("Application is loaded");
+});
