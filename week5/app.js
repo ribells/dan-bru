130571f65c8212faae036790b49ba353060501c1
ribells <bcampbel01@risd.edu>
2021-09-18 11:03:58 -0400

    week5 example of three.js in NPM

---
+++ week5/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(__dirname + '/public'))
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/terrain_vis/build')));
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
+
+app.listen(3000, () =>
+  console.log('Visit http://127.0.0.1:3000')
+);
